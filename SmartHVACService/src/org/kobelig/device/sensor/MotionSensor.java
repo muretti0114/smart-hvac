cda715601fb3695c348a48827cc1513cc80a7965
muretti0114 <muretti0114@gmail.com>
2015-09-01 11:26:09 +0200

    Added MotionSensor

---
+++ SmartHVACService/src/org/kobelig/device/sensor/MotionSensor.java
@@ -0,0 +1,9 @@
+package org.kobelig.device.sensor;
+
+public class MotionSensor extends WebServiceSensor {
+	public static final String property = "motion";
+
+	public MotionSensor(String id) {
+		super(id);
+	}
+}
